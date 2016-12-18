38e3de7c8137b1f62f1a07269c7aa8682544a899
Gisellabella <gisella@gmail.com>
2016-12-18 15:28:55 -0700

    the loan calculator seems funky, did i forget a step?

--- challenge_2/js/app.js
+++ challenge_2/js/app.js
@@ -1,8 +1,48 @@
 console.log('challenge #2 -- app.js loaded!');
 
-// var app = angular.module("appTwo", []);
+var app = angular.module("appTwo", []);
 // app.controller("WelcomeController", WelcomeController);
 
 // function WelcomeController(){
 //   this.full_name = "YOUR NAME"
 // }
+
+
+app.controller("WelcomeController", WelcomeController);
+
+function WelcomeController(){
+	this.full_name ="Gisella";
+	this.age="48";
+	this.city="Saratoga Springs";
+	this.state="NY";
+	this.shout = function(name){
+	    return name.toUpperCase() + "!";
+		};
+
+}
+
+  app.controller("ClassController", ClassController);
+
+function ClassController(){
+	this.class_name ="Big Nick's Cult Of Zebulon";
+	this.enrolled_students="10";
+	this.start_date="10/31/2016";
+	this.end_date="02/01/2017";
+	this.daysRemaining = function (){
+		var end = Date.parse(this.end_date);
+		var todays_date = Date.now();
+		console.log("today" + todays_date);		
+		console.log("end" + end);
+	    var days_left = (end - todays_date)/(1000*60*60*24);
+	    return days_left;
+		};
+
+}
+
+       // <li>Class Name: {{ "class_name" }}</li>
+       //  <li>Students: {{ "enrolled_students" }}</li>
+       //  <li>Start: {{ "start_date" }}</li>
+       //  <li>End: {{ "end_date" }}</li>
+       //  <!-- HINT: Create a daysRemaining() function in your controller that calculate this -->
+       //  <li>Days Left: {{ "daysRemaining()" }}</li>
+
--- challenge_3/js/app.js
+++ challenge_3/js/app.js
@@ -1,9 +1,23 @@
+
 console.log('challenge #3 -- app.js loaded!');
 
-// var app = angular.module("appThree", []);
-// app.controller("infoController", infoController)
+var app = angular.module("appThree", []);
+app.controller("infoController", infoController);
 
-// function infoController(){
-//   this.name = "Jon Doe"; // default name
-//   this.height = 5.6; // default height in feet
-// }
+function infoController(){
+  this.name = "Jon Doe"; // default name
+  this.height = 5.6; // default height in feet
+}
+function loanController(){
+  this.amount = (100) ;
+  this.apr =(5);
+  this.month = function(n){
+    var annual_interest = (this.amount * (this.apr / 100));
+    var monthly_interst = (annual_interest / 12);
+    var accrued_interest = n * monthly_interest ;
+    return {
+      accrued: accrued_interest,
+      total_balance: this.amount + accrued_interest
+    };
+  };
+}
